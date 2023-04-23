30b030e0aa02bf4bb90605274df5273d666e2403
Tim Holleque <timothy.t.holleque@gmail.com>
2023-04-23 16:19:14 -0400

    Initial commit

---
+++ Circle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleObjects
+{
+
+    internal class Circle
+    {
+        //Properity(s)
+        private double Radius { get; set; }
+        // Constructor
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+        //Methods 
+        public double CalculateDiameter()
+        {
+            return Radius * 2;
+        }
+        public double CalculateCircumference()
+        {
+            return 2 * Math.PI * Radius;
+        }
+        public double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+        public void Grow()
+        {
+            Radius *= 2;
+        }
+        public double GetRadius() 
+        { 
+            return Radius;
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿namespace CircleObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Cirle Tester!");
+            Console.WriteLine("Enter a radius:");
+            bool goOn = true;
+            while (goOn) { 
+            bool tryRad = false;
+
+                while (tryRad == false)
+                {
+                    try
+                    {
+                        tryRad = true;
+                        double radius = double.Parse(Console.ReadLine());
+                        Circle c = new Circle(radius);
+                        Console.WriteLine("Diameter: " + c.CalculateDiameter());
+                        Console.WriteLine("Circumference: " + c.CalculateCircumference());
+                        Console.WriteLine("Area: " + c.CalculateArea());
+
+                        Console.WriteLine("Should the circle grow?");
+                        string grow = Console.ReadLine();
+                        if (grow == "y")
+                        {
+                            c.Grow();
+                            Console.WriteLine("Diameter: " + c.CalculateDiameter());
+                            Console.WriteLine("Circumference: " + c.CalculateCircumference());
+                            Console.WriteLine("Area: " + c.CalculateArea());
+                            
+                        }
+                        else
+                        {
+                            double newRad = c.GetRadius();
+                            Console.WriteLine("Goodbye! The circle's radius is: " + newRad);
+                        }
+                    }
+                    catch (FormatException e)
+                    {
+                        Console.WriteLine(e.Message);
+                        Console.WriteLine("Please enter a valid number:");
+                    }
+                }
+            }
+        }
+    }
+}
